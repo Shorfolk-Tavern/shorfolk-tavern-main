a291fdb4b5bb609869f3af803093c81c28976454
V0ltaroo <nendorans@gmail.com>
2023-01-26 16:54:20 +0100

    Initial commit

---
+++ apps/shorfolk-tavern/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shorfolk-tavern-main-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'shorfolk-tavern';
+}
